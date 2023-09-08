0fd96d94db072950f2e226af76a791c5ee527f37
Silva <john.silva@capgemini.com>
2023-09-08 17:50:27 -0300

    Class number 35 - Two way data bindings

--- lwc learning/force-app/main/default/lwc/helloWorld/helloWorld.js
+++ lwc learning/force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -2,4 +2,21 @@ import { LightningElement } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
     fullName = 'Zero to Hero';
+    title = 'aura';
+
+    changeHandler(event) {
+        if (event.target.value.length) {
+            this.title = event.target.value;
+
+            return this;
+        }
+
+        this.title = 'aura';
+
+        return this;
+    }
+
+    address = {
+        
+    }
 }
